421c6e5852148f7622cc8f09ca3bb75564c73648
kaunild <dhruv.kaunil@gmail.com>
2019-03-26 17:09:06 -0600

    Installed blueprint and created a MapMain component

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,39 +1,11 @@
 /// app.js
 import React from 'react';
-import DeckGL, {LineLayer} from 'deck.gl';
-import {StaticMap} from 'react-map-gl';
-
-// Set your mapbox access token here
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoia2F1bmlsLWRocnV2IiwiYSI6ImNqdHA3djZhYTAxdmw0YXJ2Nm9nZWZpdTMifQ.i2khisdjFR-fPCZ421loYg';
-
-// Initial viewport settings
-const initialViewState = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0
-};
-
-// Data to be used by the LineLayer
-const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
-
+import MapMain from './components/MapMain'
 class App extends React.Component {
   render() {
-    const layers = [
-      new LineLayer({id: 'line-layer', data})
-    ];
 
     return (
-      <DeckGL
-        initialViewState={initialViewState}
-        controller={true}
-        layers={layers}
-      >
-        <StaticMap mapboxApiAccessToken={
-          MAPBOX_ACCESS_TOKEN
-        } />
-      </DeckGL>
+      <MapMain />
     );
   }
 }
---
+++ web-app/src/components/index.js
@@ -0,0 +1,1 @@
+export {default as MapMain} from './MapMain/MapMain'
