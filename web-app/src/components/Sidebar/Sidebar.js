3680fad1bba52d96f4c77998f1fe2047488e73e3
kaunild <dhruv.kaunil@gmail.com>
2019-03-27 11:55:50 -0600

    added sidebar

--- web-app/src/components/Sidebar/Sidebar.js
+++ web-app/src/components/Sidebar/Sidebar.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import {
+  Tab,
+  Tabs,
+  ButtonGroup,
+  Button,
+  AnchorButton
 } from "@blueprintjs/core";
 
 import './Sidebar.css'
@@ -9,19 +14,50 @@ class Sidebar extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      isOpen: true
+      layersIsVisible: true,
+      filtersIsVisible: false
     }
 
-    this.toggleDrawer = this.toggleDrawer.bind(this)
+    this.toggleLayers = this.toggleLayers.bind(this)
   }
 
-  toggleDrawer() {
-    var isOpen = !this.state.isOpen
-    this.setState({isOpen: isOpen})
+  toggleLayers() {
+    var layersIsVisible = !this.state.layersIsVisible
+    this.setState({
+      layersIsVisible: layersIsVisible,
+      filtersIsVisible: !layersIsVisible,
+
+    })
   }
   render() {
     return (
       <div className="Sidebar">
+        <div
+          style={{
+            clear:"both",
+            height:"50px",
+            width:"100%"
+
+          }}
+          className="tabButtonContainer"
+        >
+          <ButtonGroup minimal fill>
+            <Button onClick={this.toggleLayers} icon="layers" />
+            <Button onClick={this.toggleLayers} icon="filter" />
+          </ButtonGroup>
+        </div>
+        <div
+          className={this.state.layersIsVisible ? "tabContainer" : "tabContainer  tabVisible"}
+        >
+          <h1 class="bp3-dark bp3-heading">Layers</h1>
+        </div>
+
+
+        <div
+          className={this.state.layersIsVisible ? "tabContainer tabVisible" : "tabContainer "}
+        >
+          <h1 class="bp3-dark bp3-heading">Filters</h1>
+        </div>
 
       </div>
     )
