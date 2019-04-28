857bf6b10f0b934bba05040a2462a77892d5befe
BhalchandraNaik <bhna5709@colorado.edu>
2019-04-28 11:48:43 -0600

    Installed CORs

--- backend/index.js
+++ backend/index.js
@@ -12,87 +12,89 @@ const express = require('express')
 const moment = require('moment')
 const request = require('request')
 const random = require('random')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
+app.use(cors())
 app.listen(port, ()=>{
     console.log('Server is up on Port '+port)
 })
 
 
-app.get('/getcountday/:date', async (req, res)=>{
+app.get('/getcountday/:date', async (req, res, next)=>{
     date = moment(req.params.date)
     const period = moment({year:date.format('YYYY'), month:date.format('MM')-1}).format('YYYY-MM')
     try {
         const count = await NycData[period].countDocuments({tpep_pickup_datetime:{$regex : ".*"+req.params.date+".*"}}, (error, result)=>{
             if(result){
-                res.status(201).send({
+                res.status(201).json({
                     count:result,
                     date:date
                 })
             }else{
-                res.status(404).send({
+                res.status(404).json({
                     error:"Data doesn't exist! "
                 })
             }
         })
     } catch (error) {
         console.log('error occured')
-        res.status(500).send(error)
+        res.status(500).json(error)
     }
 })
 
-app.get('/getzonepickups/:date/:pu_id/:du_id', async (req, res)=>{
+app.get('/getzonepickups/:date/:pu_id/:du_id', async (req, res, next)=>{
     date = moment(req.params.date)
     const period = moment({year:date.format('YYYY'), month:date.format('MM')-1}).format('YYYY-MM')
     try {
         const count = await NycData[period].countDocuments({tpep_pickup_datetime:{$regex : ".*"+req.params.date+".*"}, PULocationID:req.params.pu_id, DOLocationID:req.params.du_id},(error, result)=>{
             if(!error){
-                res.status(201).send({
+                res.status(201).json({
                     count:result,
                     date:date
                 })
             }else{
-                res.status(404).send({
+                res.status(404).json({
                     error:error,
                     message:"Data doesn't exist!"
                 })
             }
         })
     } catch (error) {
-        res.status(500).send({
+        res.status(500).json({
             error:'An Error occured!'
         })
     }
 })
 
 
-app.get('/getwaypoints/:from_long/:from_lat/:to_long/:to_lat', async (req, res)=>{
+app.get('/getwaypoints/:from_long/:from_lat/:to_long/:to_lat', async (req, res, next)=>{
     const _url = apiUrls.waypointsURL(req.params.from_long, req.params.from_lat, req.params.to_long, req.params.to_lat, apiKey.waypoints())
     try {
         request({url:_url, json:true}, (error, response)=>{
             if(error){
-                res.status(500).send({
+                res.status(500).json({
                     message:'Service Unavailable'
                 })
             }else if(response.body.code==400){
-                res.status(400).send({
+                res.status(400).json({
                     message:'Invalid Arguments provided!'
                 })
             }else{
-                res.status(400).send({
+                res.status(201).json({
                     route:response.body
                 })
             }
         })    
     } catch (error) {
-        res.status(500).send({
+        res.status(500).json({
             error: 'An error occuered!'
         })
     }
 })
 
-app.get('/getrandomtaxirides/:date', async (req, res)=>{
+app.get('/getrandomtaxirides/:date', async (req, res, next)=>{
     const date = moment(req.params.date)
     const period = moment({year:date.format('YYYY'), month:date.format('MM')-1}).format('YYYY-MM')
     const NycGeoDataModel = NycGeoData[period]
@@ -116,30 +118,30 @@ app.get('/getrandomtaxirides/:date', async (req, res)=>{
             }).sort('pickup_datetime').lean().then((rides)=>{
                 helpers.getAllRides(rides, 0, (error, data, statusCode)=>{
                     if(error){
-                        res.status(statusCode).send({
+                        res.status(statusCode).json({
                             error:error,
                             msg:"An error occurred while Calling the API"
                         })
                     }else{
-                        res.status(statusCode).send({
+                        res.status(statusCode).json({
                             rides:data
                         })
                     }
                 })
             }).catch((error)=>{
-                res.status(500).send({
+                res.status(500).json({
                     msg:'An error occurred while fetching ride information!',
                     error:error
                 })
             })
         }).catch((error)=>{
-            res.status(500).send({
+            res.status(500).json({
                 error:error,
                 msg:"An error occured while fetching the rides for the day!"
             })
         })
     } catch (error) {
-        res.send({
+        res.json({
             msg : 'An error occurred!',
             error:error
         })
@@ -147,7 +149,7 @@ app.get('/getrandomtaxirides/:date', async (req, res)=>{
 })
 
 
-app.get('/operationalcabs/:date', async(req, res)=>{
+app.get('/operationalcabs/:date', async(req, res, next)=>{
     date = moment(req.params.date)
     const period = moment({year:date.format('YYYY'), month:date.format('MM')-1}).format('YYYY-MM')
     NycLicMedals.find({date:{
@@ -158,13 +160,13 @@ app.get('/operationalcabs/:date', async(req, res)=>{
         date:1,
         _id:false
     }).sort('date').lean().then((results)=>{
-        res.send({
+        res.status(201).json({
             msg:'Success!',
             period:period,
             results:results
         })
     }).catch((error)=>{
-        res.status(500).send({
+        res.status(500).json({
             msg:'The request failed! Cannot read the database',
             error:error
         })
