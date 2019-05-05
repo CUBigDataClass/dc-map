fe1c8cd761a8a6f0a3a9722fafd280f42cb85c04
BhalchandraNaik <bhna5709@colorado.edu>
2019-05-04 19:33:43 -0600

    Removed the limit constraint

--- backend/index.js
+++ backend/index.js
@@ -64,8 +64,7 @@ app.post('/getrides', async (req, res) => {
                 _id:0,
                 PULocationID : 1,
                 DOLocationID : 1
-            }},
-            {$limit : 10},
+            }}
             // {$sort:{"_id":1}}
         ]).then((result)=>{
             res.status(201).send({
